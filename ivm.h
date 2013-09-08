bf9bcd4734d78badba3d3345e3bfe3a10130b9b7
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2013-09-09 03:10:56 +0400

    Added virtual machine for J1

---
+++ ivm.h
@@ -0,0 +1,142 @@
+#ifndef __IVM_H__
+#define __IVM_H__
+
+#include <inttypes.h>
+
+#define OP(x)  ((x) & (0xe000))
+#define ARG(x) ((x) & (0x1fff))
+#define ARG_LIT(x) ((x) & 0x7fff)
+
+#define ALU_OP(x)   (x & (0xf << 8))
+#define ALU_DS(x)		(x & (0x3 << 0))
+#define ALU_RS(x)		(x & (0x3 << 2))
+
+static void ivm_mem_put(uint16_t addr, uint16_t value);
+static uint16_t ivm_mem_get(uint16_t addr);
+
+/* Operation types */
+enum {
+	OP_JMP  = 0x0000,
+	OP_JZ   = 0x2000,
+	OP_CALL = 0x4000,
+	OP_ALU  = 0x6000,
+	OP_LIT  = 0x8000
+};
+
+/* ALU operation types */
+enum {
+	ALU_OP_T      = 0,
+	ALU_OP_N      = 1,
+	ALU_OP_PLUS   = 2,
+	ALU_OP_AND    = 3,
+	ALU_OP_OR     = 4,
+	ALU_OP_XOR    = 5,
+	ALU_OP_NEG    = 6,
+	ALU_OP_EQ     = 7,
+	ALU_OP_LESS   = 8,
+	ALU_OP_RSHIFT = 9,
+	ALU_OP_DEC    = 10,
+	ALU_OP_R      = 11,
+	ALU_OP_MEM    = 12,
+	ALU_OP_LSHIFT = 13,
+	ALU_OP_DEPTH  = 14,
+	ALU_OP_ULESS  = 15
+};
+
+static uint16_t ivm_ds[16], ivm_rs[16]; /* data stack and return stack */
+static int8_t ivm_dp, ivm_rp; /* top of data stack, next on data stack, top of ret stack */
+static int16_t ivm_pc;
+
+static inline void ivm_reset() {
+	ivm_pc = 0;
+	ivm_dp = ivm_rp = -1;
+}
+
+static inline void ivm_step(uint16_t word) {
+	if (word & 0x8000) {
+		ivm_ds[++ivm_dp] = ARG_LIT(word);
+		ivm_pc++;
+		return;
+	}
+
+	switch (OP(word)) {
+		case OP_JZ:
+			if (ivm_ds[ivm_dp--] == 0) {
+				ivm_pc = ARG(word);
+			} else {
+				ivm_pc++;
+			}
+			break;
+		case OP_JMP:
+			ivm_pc = ARG(word);
+			break;
+		case OP_CALL:
+			ivm_rs[++ivm_rp] = ivm_pc+1;
+			ivm_pc = ARG(word);
+			break;
+		case OP_ALU: {
+			uint16_t t, n, r, res;
+			t = ivm_ds[ivm_dp];
+			if (ivm_dp > 0) {
+				n = ivm_ds[ivm_dp-1];
+			}
+			r = ivm_rs[ivm_rp];
+
+			ivm_pc++;
+
+			switch (ALU_OP(word) >> 8) {
+				case ALU_OP_T: res = t; break;
+				case ALU_OP_N: res = n; break;
+				case ALU_OP_PLUS: res = t + n; break;
+				case ALU_OP_AND: res = t & n; break;
+				case ALU_OP_OR: res = t | n; break;
+				case ALU_OP_XOR: res = t ^ n; break;
+				case ALU_OP_NEG: res = ~t; break;
+				case ALU_OP_EQ: res = (t == n); break;
+				case ALU_OP_LESS: res = (n < t); break;
+				case ALU_OP_RSHIFT: res = (n >> t); break;
+				case ALU_OP_DEC: res = t-1; break;
+				case ALU_OP_R: res = r; break;
+				case ALU_OP_MEM: res = ivm_mem_get(t); break;
+				case ALU_OP_LSHIFT: res = (n << t); break;
+				case ALU_OP_DEPTH: res = ivm_dp+1; break;
+				case ALU_OP_ULESS: res = (n < t); break;
+			}
+
+			switch (ALU_DS(word)) {
+				case (1 << 0): ivm_dp++; break;
+				case (2 << 0): ivm_dp--; break;
+			}
+
+			switch (ALU_RS(word)) {
+				case (1 << 2): ivm_rp++; break;
+				case (2 << 2): ivm_rp--; break;
+			}
+
+			if (word & (1 << 5)) {
+				ivm_mem_put(t, n);
+			}
+			if (word & (1 << 6)) {
+				ivm_rs[ivm_rp] = t;
+			}
+			if (word & (1 << 7)) {
+				ivm_ds[ivm_dp-1] = t;
+			}
+			if (word & (1 << 12)) {
+				/* TODO: set 0x7fff return address on reset? */
+				if (ivm_rp < 0) { /* exit condition */
+					ivm_pc = 0x7fff;
+				} else {
+					ivm_pc = r;
+				}
+			}
+
+			if (ivm_dp >= 0) {
+				ivm_ds[ivm_dp] = res;
+			}
+			break;
+		}
+	}
+}
+
+#endif /* __IVM_H__ */
--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <strings.h>
 #include "launcher.h"
-#include "dcpu16.h"
+#include "ivm.h"
+
+uint16_t ram[65536];
+
+static void ivm_mem_put(uint16_t addr, uint16_t value)
+{
+	ram[addr]=value;
+	if(addr<(65536/2))
+	{
+		printf("Written mem[%08x]=%08x\n",addr,value);
+	} else
+	{
+		printf("Written mem[%08x]=%08x\n",addr,value);
+	}
+};
+static uint16_t ivm_mem_get(uint16_t addr)
+{
+	return ram[addr];
+};
 
 int main( int argc, char **argv )
 {
-//  hardware_init(  );            //Here initialise either hardware or emulator
-  context_t context;
-  memset( &context, 0, sizeof( context ) );
+  int i = 0;
+  i = read(STDIN_FILENO, ram, sizeof(ram));
+  printf("loaded %d bytes\n",i);
+  ivm_reset();
   while( 1 ) {
-    process_one_instruction( &context );
-    print_status( &context );
-//    launcher(  );
+    ivm_step(ram[ivm_pc]);
+    printf("ivm_pc=%04x\tivm_dp=%08x\tivm_rs=%08x\n",ivm_pc,ivm_dp,ivm_rs);
   };
   return 0;
 };
