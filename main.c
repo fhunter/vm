f901cdaf64a9b70505642c8a01669ea030142343
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2013-09-10 04:51:18 +0400

    Implemented rudimentary video driver

--- main.c
+++ main.c
@@ -4,15 +4,25 @@
 #include "launcher.h"
 #include "ivm.h"
 
+#define RAMSIZE	65536
+#define PAGESIZE 256
+
 uint16_t ram[65536];
+uint8_t ram_bitmap[RAMSIZE/PAGESIZE/(sizeof(uint8_t)*8)];
 
 static void ivm_mem_put(uint16_t addr, uint16_t value)
 {
 	if(addr<0xfc00){
 		//Access RAM
 		ram[addr]=value;
+		//Update bitmap
+//		set_bit(ram_bitmap,addr);
 	} else
 	{
+		if(addr>0xff00){
+			printf("\033[%d;%dH%c",(0xffff-addr)/16,(0xffff-addr)%16,value);
+			//Video RAM
+		}
 		//Access peripherials
 	}
 };
@@ -37,7 +47,7 @@ int main( int argc, char **argv )
   i = 0;
   while( 1 ) {
     ivm_step(ram[ivm_pc]);
-    printf("i=%04x ivm_pc=%04x\tivm_dp=%08x\tivm_rs=%08x\n",i,ivm_pc,ivm_dp,ivm_rs);
+    printf("\033[18;1H i=%04x ivm_pc=%04x\tivm_dp=%08x\tivm_rs=%08x\n",i,ivm_pc,ivm_dp,ivm_rs);
     usleep(100000);
     i++;
   };
