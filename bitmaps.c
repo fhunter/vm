ea65a92c694e8744293e27c06e17e43de34cc363
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2013-09-11 01:52:50 +0400

    Added doxygen tags

--- bitmaps.c
+++ bitmaps.c
@@ -4,14 +4,32 @@
   */
 #include "bitmaps.h"
 
+/** \brief Sets bit in bitmap
+  * \param map -- pointer to bitmap
+  * \param addr -- address for which to set the bit in bitmap
+  * \todo Implement
+  */
 void set_bit( uint8_t * map, uint16_t addr )
 {
 };
 
+/** \brief Clears bit in bitmap
+  * \param map -- pointer to bitmap
+  * \param addr -- address for which to clear the bit in bitmap
+  * \todo Implement
+  */
 void clr_bit( uint8_t * map, uint16_t addr )
 {
 };
 
+/** \brief Checks if a bit in bitmap is set
+  * \param map -- pointer to bitmap
+  * \param addr -- address for which to check the bit in bitmap
+  * \return 0 -- if the bit is not set
+  * \return non-zero -- if the bit is set
+  * \todo Implement
+  */
 uint8_t get_bit( uint8_t * map, uint16_t addr )
 {
+  return 1;
 };
