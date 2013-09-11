fcfd3a27ffc5b4701b4998a46b7f789dd90f26be
Nikolaj Zamotaev <fhunter@fhunter.mine.nu>
2013-09-11 19:49:25 +0400

    Implemented bitmap operations (fixme, with hardcoded pagesize)

--- bitmaps.c
+++ bitmaps.c
@@ -4,6 +4,23 @@
   */
 #include "bitmaps.h"
 
+/** \brief Converts from page size to number of bits to address contents of the page
+  * \param pagesize -- size of the page
+  * \return number of bits for addressing page contents
+  * \note pagesize must be a power of 2
+  * \note pagesize must be lesser than 2^32
+  */
+static uint8_t pagesize2bits( uint16_t pagesize )
+{
+  uint8_t i;
+  for( i = 0; i < 32; i++ ) {
+    if( pagesize == 0 )
+      return i;
+    pagesize >>= 1;
+  };
+  return 32;
+};
+
 /** \brief Sets bit in bitmap
   * \param map -- pointer to bitmap
   * \param addr -- address for which to set the bit in bitmap
@@ -11,6 +28,9 @@
   */
 void set_bit( uint8_t * map, uint16_t addr )
 {
+  uint32_t temp = 0;
+  temp = addr >> pagesize2bits( 256 );
+  map[temp >> 3] |= ( 1 << ( temp & 0x07u ) );
 };
 
 /** \brief Clears bit in bitmap
@@ -20,6 +40,9 @@ void set_bit( uint8_t * map, uint16_t addr )
   */
 void clr_bit( uint8_t * map, uint16_t addr )
 {
+  uint32_t temp = 0;
+  temp = addr >> pagesize2bits( 256 );
+  map[temp >> 3] &= ~( 1 << ( temp & 0x07u ) );
 };
 
 /** \brief Checks if a bit in bitmap is set
@@ -31,5 +54,7 @@ void clr_bit( uint8_t * map, uint16_t addr )
   */
 uint8_t get_bit( uint8_t * map, uint16_t addr )
 {
-  return 1;
+  uint32_t temp = 0;
+  temp = addr >> pagesize2bits( 256 );
+  return map[temp >> 3] | ( 1 << ( temp & 0x07u ) );
 };
