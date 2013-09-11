346d049c658616a3f2852689bc1dc2b6b485704b
Nikolaj Zamotaev <fhunter@fhunter.mine.nu>
2013-09-11 17:50:39 +0400

    Added template for functions to save and restore virtual machine

---
+++ state.c
@@ -0,0 +1,6 @@
+/** \file state.c
+  * \brief Save/load virtual machine states
+  * \author Nikolay Zamotaev
+  */
+
+#include "state.h"
---
+++ state.h
@@ -0,0 +1,9 @@
+/** \file state.h
+  * \brief Save/load virtual machine states
+  * \author Nikolay Zamotaev
+  */
+
+#ifndef _STATE__H_
+#define _STATE__H_
+
+#endif
