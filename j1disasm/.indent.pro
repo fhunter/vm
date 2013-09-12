c2143648808d26747ac1117043059bef6e47d984
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2013-09-12 04:01:10 +0400

    Added simple disassembler

---
+++ j1disasm/.indent.pro
@@ -0,0 +1,3 @@
+-npsl -br -i 2 -bli 0 -lc 70 -l 70 -nut -cli2 -prs -nsai -nsaw -nsaf
+-npcs -sob
+
---
+++ j1disasm/disassembler.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+char *ops[] = {
+  "T   ",
+  "N   ",
+  "T+N ",
+  "T&N ",
+  "T|N ",
+  "T^N ",
+  "~T  ",
+  "N=T ",
+  "N<T ",
+  "N>>T",
+  "T-1 ",
+  "R   ",
+  "[T] ",
+  "N<<T",
+  "dpth",
+  "Nu<T",
+};
+
+int main( int argc, char **argv )
+{
+  uint16_t buffer[8192] = { 0 };
+  uint16_t length = 0;
+  uint16_t i;
+  uint16_t code;
+  length = read( 0, buffer, sizeof( buffer ) );
+  length >>= 1;                 //uint16
+  for( i = 0; i < length; i++ ) {
+    printf( "%04x\t", i );
+    code = buffer[i];
+    if( code & 0x8000 ) {
+      //literal
+      printf( "lit %04x (%d)", code & 0x7fff, code & 0x7fff );
+    }
+    else {
+      switch ( code >> 13 ) {
+        case 0:
+          {
+            printf( "jump %04x", code & 0x1fff );
+            break;
+          }
+        case 1:
+          {
+            printf( "jmpc %04x", code & 0x1fff );
+            break;
+          }
+        case 2:
+          {
+            printf( "call %04x", code & 0x1fff );
+            break;
+          }
+        case 3:
+          {
+            printf( "alu  %s T'=%s %s %s %s rstack+=%d dstack+=%d",
+                    code & ( 1 << 12 ) ? "r->pc" : "     ",
+                    ops[( code >> 8 ) & 0x0f],
+                    ( code & ( 1 << 7 ) ) ? "T->N" : "    ",
+                    ( code & ( 1 << 6 ) ) ? "T->R" : "    ",
+                    ( code & ( 1 << 5 ) ) ? "N->[T]" : "      ",
+                    ( ( int ) code & 0x0c ) >> 2,
+                    ( ( int ) code & 0x03 ) );
+            break;
+          };
+        default:
+          {
+            printf( "unknown: %04x", code );
+          }
+      };
+    };
+    printf( "\n" );
+  }
+};
