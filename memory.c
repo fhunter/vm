6ad41aafbddb095475df740db62fe6d6c32e1dea
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2013-10-27 04:12:28 +0400

    Added preliminary defines for peripherials

--- memory.c
+++ memory.c
@@ -22,6 +22,50 @@ void reinit_ram( struct t_virtual_machine *machine )
   bitmap_clearall( machine->ram_bitmap );
 };
 
+static void ram_write( struct t_virtual_machine *machine,
+                       uint16_t addr, uint16_t value )
+{
+  if( bitmap_get( machine->ram_bitmap, addr ) ) {
+    //the memory was already accessed
+    if( machine->ram_pointers[addr >> 8] == NULL ) {
+      //memory is swapped out
+      //need to get it from disk
+      //FIXME: not implemented
+    }
+    else {
+      //memory is available
+      machine->ram_pointers[addr >> 8][addr & 0xffu] = value;
+    }
+  }
+  else {
+    machine->ram_pointers[addr >> 8] =
+      calloc( PAGESIZE, sizeof( uint16_t ) );
+    machine->ram_pointers[addr >> 8][addr & 0xfful] = value;
+    //Update bitmap
+    bitmap_set( machine->ram_bitmap, addr );
+  }
+};
+
+static uint16_t ram_read( struct t_virtual_machine *machine,
+                          uint16_t addr )
+{
+  if( bitmap_get( machine->ram_bitmap, addr ) ) {
+    if( machine->ram_pointers[addr >> 8] == NULL ) {
+      //Address not found, must fetch from disk.
+      //Unimplemented
+      return 0;
+    }
+    else {
+      //Address found
+      return machine->ram_pointers[addr >> 8][addr & 0xfful];
+    }
+  }
+  else {
+    //Page was never accessed for write. Memory is empty
+    return 0;
+  }
+};
+
 /** \brief Write the word to memory address
   * \param machine -- virtual machine state for the operation
   * \param addr -- write address
@@ -30,38 +74,33 @@ void reinit_ram( struct t_virtual_machine *machine )
   * \todo Untested
   */
 void ivm_mem_put( struct t_virtual_machine *machine,
-                         uint16_t addr, uint16_t value )
+                  uint16_t addr, uint16_t value )
 {
-  if( addr < 0xfc00 ) {
+  if( addr < P_PERIPH_BASE ) {
     //Access RAM
-    if( bitmap_get( machine->ram_bitmap, addr ) ) {
-      //the memory was already accessed
-      if( machine->ram_pointers[addr >> 8] == NULL ) {
-        //memory is swapped out
-        //need to get it from disk
-        //FIXME: not implemented
-      }
-      else {
-        //memory is available
-        machine->ram_pointers[addr >> 8][addr & 0xffu] = value;
-      }
-    }
-    else {
-      machine->ram_pointers[addr >> 8] =
-        calloc( PAGESIZE, sizeof( uint16_t ) );
-      machine->ram_pointers[addr >> 8][addr & 0xfful] = value;
-      //Update bitmap
-      bitmap_set( machine->ram_bitmap, addr );
-    }
+    ram_write( machine, addr, value );
   }
   else {
     //Peripherial region
-    //Video RAM
-    if( addr > 0xff00 ) {
+    // buttons, syscall, etc
+    if( addr < ( P_BUTTONS_BASE + P_BUTTONS_LENGTH ) ) {
+    }
+    else if( addr < ( P_SYSCALL_BASE + P_SYSCALL_LENGTH ) ) {
+    }
+    else if( addr < ( P_TIMER_BASE + P_TIMER_LENGTH ) ) {
+    }
+    else if( ( addr > P_VIDEO_BASE )
+             && ( addr <= ( P_VIDEO_BASE + P_VIDEO_LENGTH ) ) ) {
+      //Video RAM
+      ram_write( machine, addr, value );
+      //FIXME: add abstraction
       printf( "\0337\033[%d;%dH%c\0338", ( 0xffff - addr ) / 16,
               ( 0xffff - addr ) % 16, value );
     }
-    //Access peripherials
+    else {
+      //Peripherial undefined
+      ram_write( machine, addr, value );
+    }
   }
 };
 
@@ -74,28 +113,33 @@ void ivm_mem_put( struct t_virtual_machine *machine,
   * \note Returns 0, if the page was never accessed
   */
 uint16_t ivm_mem_get( struct t_virtual_machine *machine,
-                             uint16_t addr )
+                      uint16_t addr )
 {
   if( addr < 0xfc00 ) {
     //Access RAM
-    if( bitmap_get( machine->ram_bitmap, addr ) ) {
-      if( machine->ram_pointers[addr >> 8] == NULL ) {
-        //Address not found, must fetch from disk.
-        //Unimplemented
-        return 0;
-      }
-      else {
-        //Address found
-        return machine->ram_pointers[addr >> 8][addr & 0xfful];
-      }
-    }
-    else {
-      //Page was never accessed for write. Memory is empty
-      return 0;
-    }
+    return ram_read( machine, addr );
   }
   else {
-    //Access peripherials
-    return 0;
+    //Peripherial region
+    // buttons, syscall, etc
+    if( addr < ( P_BUTTONS_BASE + P_BUTTONS_LENGTH ) ) {
+      return 0;
+    }
+    else if( addr < ( P_SYSCALL_BASE + P_SYSCALL_LENGTH ) ) {
+      return 0;
+    }
+    else if( addr < ( P_TIMER_BASE + P_TIMER_LENGTH ) ) {
+      return 0;
+    }
+    else if( ( addr > P_VIDEO_BASE )
+             && ( addr <= ( P_VIDEO_BASE + P_VIDEO_LENGTH ) ) ) {
+      //Video RAM
+      return ram_read( machine, addr );
+    }
+    else {
+      //Peripherial undefined
+      return ram_read( machine, addr );
+    }
   }
+  return 0;
 };
--- peripherials/peripherials.h
+++ peripherials/peripherials.h
@@ -7,4 +7,15 @@
 #include "lcd.h"
 #include "buttons.h"
 
+#define P_PERIPH_BASE	0xfc00
+#define P_BUTTONS_BASE	P_PERIPH_BASE
+#define P_BUTTONS_LENGTH	16
+#define P_SYSCALL_BASE	(P_BUTTONS_BASE+ P_BUTTONS_LENGTH)
+#define P_SYSCALL_LENGTH 128
+#define P_TIMER_BASE	(P_SYSCALL_BASE+ P_SYSCALL_LENGTH)
+#define P_TIMER_LENGTH  16
+//FIXME: add assert to check that video RAM is not overrun
+#define P_VIDEO_BASE	0xfe00
+#define P_VIDEO_LENGTH  0x200
+
 #endif
